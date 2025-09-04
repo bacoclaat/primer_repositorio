4d4efbd0a2f5c3a91f143b06689ee2154fbfdb25
Baco <ignacio.leyton.cornejo@gmail.com>
2025-09-04 14:55:20 -0400

    agregado menu

--- main.py
+++ main.py
@@ -3,13 +3,24 @@
 import agenda
 while True:
     print(" ~~ MENU ~~ \n" \
-    "\n" \
-    "1. Agregar contacto." \
-    "2. Listar contacto." \
-    "3. Buscar contacto" \
+    "\n" 
+    "1. Agregar contacto.\n" 
+    "2. Listar contacto.\n" 
+    "3. Buscar contacto.\n" 
     "4. Salir.")
-    decision = input("Eliga una opcion")
+    decision = input("Eliga una opcion: ")
     if decision == "1":
         nombre = input("Dame el nombre: ")
         telefono = input("Dame el telefono: ")
-        agregar_contacto(nombre,telefono)
+        agenda.agregar_contacto(nombre,telefono)
+        input("Aprete cualquier tecla...")
+    if decision == "2":
+        agenda.listar_contactos()
+        input("Aprete cualquier tecla...")
+    if decision == "3":
+        nombre = "Dame el nombre: "
+        agenda.buscar_contacto(nombre)
+        input("Aprete cualquier tecla...")
+    if decision == "4":
+        print("Adios!")
+        break
